4b7bdd203c76428254ada4d38f3428706d42e702
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-05-22 15:17:14 +0600

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from  '../../images/logo.png';
+
+const Header = () => {
+    return (
+        <div>
+            <img style={{height:75}} src={logo} alt="" />
+        </div>
+    );
+};
+
+export default Header;
