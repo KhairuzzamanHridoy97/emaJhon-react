d73d7c75562cbc2713e54c109f6c102577eacb90
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-05-25 15:07:30 +0600

    setProducts

--- front/src/Components/Shop/Shop.js
+++ front/src/Components/Shop/Shop.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React ,{useState,useEffect}from 'react';
 import './Shop.css';
 
 
 const Shop = () => {
+    const [products,setProducts] = useState([]);
+
+    useEffect(()=>{
+        fetch('./products.JSON')
+        .then(res=>res.json())
+        .then(data=>setProducts(data))
+    },[])
     return (
         <div className='shop-container'>
+
             <div className='product-container'>
-                <h3>Prodducts</h3>
+                <h3>Products: {products.length} </h3>
+                {
+                    products.map(
+                        product=>console.log(product)
+                    )
+                }
             </div>
-            <div>
-                Cart Container
+            <div className="cart-container">
+                <h3>Order Summary</h3>
+                <h5>Items Ordered: </h5>
             </div>
+
         </div>
     );
 };
