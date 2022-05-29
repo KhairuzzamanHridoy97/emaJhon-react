a75046658b6de07d5028ae5b3e6f7f165b89502f
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-05-29 12:48:41 +0600

    set tax & grandTotal in cart.js

--- front/src/Components/Cart/Cart.js
+++ front/src/Components/Cart/Cart.js
@@ -7,12 +7,19 @@ const Cart = (props) => {
     let total =0;
     for(const product of cart){
         total = total + product.price;
-    }
+    };
+
+    const shipping = 0;
+    const tax = (total+shipping)/100;
+    const grandTotal = total+shipping+tax;
+
+
     return (
         <div>
              <h3>Order Summary</h3>
                 <h5>Items Ordered: {props.cart.length} </h5>
                <p>Total: {total}</p> 
+               <p>Tax: {tax}</p>
         </div>
     );
 };
