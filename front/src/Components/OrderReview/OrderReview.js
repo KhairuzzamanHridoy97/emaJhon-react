fc617146827dd5011e52c8704c68a9a9f2ee77c0
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-05-31 23:00:31 +0600

    set useCart in OrderReview

--- front/src/Components/OrderReview/OrderReview.js
+++ front/src/Components/OrderReview/OrderReview.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import useProducts from '../hooks/useProducts';
+import useCart from '../hooks/useCart';
 
 const OrderReview = () => {
     const [products] = useProducts();
+    const [cart] = useCart(products);
     return (
         <div>
             <h2>{products.length}</h2>
+            <h3>{cart.length}</h3>
             <h2>This is Overview</h2>
         </div>
     );
