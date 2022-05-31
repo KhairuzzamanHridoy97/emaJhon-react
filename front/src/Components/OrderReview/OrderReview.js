8bcd3dc5e02c37264e1998653d81d31cbe73365e
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-05-31 22:42:24 +0600

    set useProducts in orderReview

--- front/src/Components/OrderReview/OrderReview.js
+++ front/src/Components/OrderReview/OrderReview.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import useProducts from '../hooks/useProducts';
 
 const OrderReview = () => {
+    const [products] = useProducts();
     return (
         <div>
+            <h2>{products.length}</h2>
             <h2>This is Overview</h2>
         </div>
     );
